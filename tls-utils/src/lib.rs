3e68c419026383e1a5486274c9dfdde2d8219a4e
Alex Pyattaev <alex.pyattaev@gmail.com>
2024-12-02 15:53:39 +0200

    address comments by Kirill Lykov

--- tls-utils/src/lib.rs
+++ tls-utils/src/lib.rs
@@ -1,5 +1,5 @@
 //! Collection of TLS related code fragments that end up popping up everywhere where quic is used.
-//! Aggregated here to avoid bugs due to conflicting implementations of the same functionality
+//! Aggregated here to avoid bugs due to conflicting implementations of the same functionality.
 
 mod tls_certificates;
 pub use tls_certificates::*;
