24eec971720ac178d3af1177a2c2e5075a1a47f6
Alex Pyattaev <alex.pyattaev@anza.xyz>
2025-02-11 19:07:01 +0000

    turbine port

--- wire_format/src/main.rs
+++ wire_format/src/main.rs
@@ -1,19 +1,24 @@
 #![allow(clippy::arithmetic_side_effects)]
 use {
     crate::gossip::*,
+    anyhow::Context,
     clap::{Parser, Subcommand, ValueEnum},
     signal_hook::{consts::SIGINT, iterator::Signals},
     std::{
         error::Error, ffi::CString, net::Ipv4Addr, path::PathBuf, sync::atomic::AtomicBool, thread,
         time::Duration,
     },
+    turbine::find_turbine_port,
 };
 
 mod gossip;
+mod pcap;
+mod turbine;
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 enum WireProtocol {
     Gossip,
+    Turbine,
 }
 
 #[derive(Parser)]
@@ -37,7 +42,11 @@ enum Commands {
         #[arg(short, long, default_value_t = 8001)]
         port: u16,
         #[arg(short, long)]
+        /// Directory with pcap files to write. Existing ontents will be destroyed!
         output: PathBuf,
+        #[arg(short, long, default_value_t = 512)]
+        /// Rough number of pacekts to capture (exact number will depend on the protocol)
+        size_hint: usize,
     },
     Parse {
         #[arg(short, long)]
@@ -48,15 +57,20 @@ enum Commands {
 pub static EXIT: AtomicBool = AtomicBool::new(false);
 
 fn main() -> Result<(), Box<dyn Error>> {
+    solana_logger::setup_with("info,solana_metrics=error");
     let mut signals = Signals::new([SIGINT])?;
 
     thread::spawn(move || {
         if let Some(sig) = signals.forever().next() {
             println!("Received signal {:?}", sig);
             EXIT.store(true, std::sync::atomic::Ordering::Relaxed);
+            // Wait for workers to ack that they are exiting
             thread::sleep(Duration::from_secs(1));
-            println!("Timed out waiting for process exit, aborting!");
-            std::process::exit(1);
+
+            if crate::EXIT.load(std::sync::atomic::Ordering::Relaxed) {
+                println!("Timed out waiting for capture to stop, aborting!");
+                std::process::exit(1);
+            }
         }
     });
     let cli = Cli::parse();
@@ -67,12 +81,21 @@ fn main() -> Result<(), Box<dyn Error>> {
             ip_addr,
             port,
             output,
+            size_hint,
         } => {
             let interface = CString::new(interface).unwrap();
             let t = std::time::Instant::now();
+            let _ = std::fs::create_dir(&output);
             let stats = match cli.protocol {
                 WireProtocol::Gossip => {
-                    capture_gossip(&interface, ip_addr, port, output).expect("Capture failed")
+                    capture_gossip(&interface, ip_addr, port, output, size_hint)
+                        .context("Capture failed")?
+                }
+                WireProtocol::Turbine => {
+                    let port = find_turbine_port("9KTG5wAR4KYFdwVDbpHAqAzjn5K5wNGvajG8Y1ki6zpt")?;
+                    println!("Got port {port}");
+                    //capture_turbine()
+                    Stats::default()
                 }
             };
 
@@ -85,7 +108,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 (stats.valid as f64 / time.as_secs_f64()) as u64
             );
         }
-        Commands::Parse { input } => todo!(),
+        Commands::Parse { input } => todo!("Parsing mode is coming up"),
     }
     //let s = serde_json::to_string_pretty(&p).unwrap();
     //println!("hi {s}");
