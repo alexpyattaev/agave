a79f9a247bb279717c9a3e961edf8584c0de8ec8
sol <sol@sola.na>
2025-08-05 20:44:18 +0000

    less ugly script

--- wire_format/parse_arrival_times.py
+++ wire_format/parse_arrival_times.py
@@ -6,7 +6,7 @@ input_file = 'monitor_data/time_log.csv'
 fieldnames = ['event_type', 'slot_number', 'index', 'fec_index', 'sender_ip', 'us_since_epoch']
 
 
-target_ips = {'192.168.1.10':0,'192.168.1.11':1}
+target_ips = {'64.130.43.178':0,'64.130.42.163':1}
 message_times= [dict(), dict()]
 
 # Open and parse the file
@@ -32,26 +32,28 @@ with open(input_file, 'r', newline='') as f:
 all_keys = set(message_times[0].keys()).union( message_times[1].keys())
 all_keys = sorted(all_keys)
 
-lost_a = 0
-lost_b = 0
-lead_a = 0
-lead_b = 0
+got = 0
+lost_0 = 0
+lost_1 = 0
+lead_0 = 0
+lead_1 = 0
 
 for key in all_keys:
 
-    a_time = message_times[0].get(key)
-    b_time = message_times[1].get(key)
-    if a_time is None:
-        lost_a += 1
+    time_0 = message_times[0].get(key)
+    time_1 = message_times[1].get(key)
+    if time_0 is None:
+        lost_0 += 1
         continue
-    if b_time is None:
-        lost_b += 1
+    if time_1 is None:
+        lost_1 += 1
         continue
-
-    diff = a_time - b_time
-    if diff > 0 : # a is slower than b
-        lead_b += diff
+    got += 1 
+    diff = time_0 - time_1
+    if diff > 0 : # 0 is slower than 1
+        lead_1 += diff
     else:
-        lead_a += -diff
+        lead_0 += -diff
 
-print(f"{lost_a=} {lost_b=} {lead_a=} {lead_b=}")
+print(f"IP addresses are {target_ips}")
+print(f"Captured {got} shreds, missing packets from 0:{lost_0} 1:{lost_1}, timing lead: 0:{lead_0}us 1:{lead_1}us")
