c4066e6479ad8f53e7fd5a43b1f784acce99cad8
Alex Pyattaev <alex.pyattaev@gmail.com>
2025-11-20 21:37:30 +0200

    grepper script

---
+++ wire_format/grepper.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import argparse
+import numpy as np
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", help="data file path", type=str)
+    parser.add_argument("slot", help="slot to locate", type=int)
+    args = parser.parse_args()
+
+    dtype = np.dtype(
+        [
+            ("time_stamp", "<u8"),
+            ("slot_number", "<u8"),
+            ("index", "<u4"),
+            ("sender_ip", "<u4"),
+            ("is_repair", "u1"),
+        ]
+    )
+    arr = np.fromfile(args.path, dtype=dtype)
+    arr = arr[arr["slot_number"] == args.slot]
+    np.save(f"{args.path}_{args.slot}")
+
+
+if __name__ == "__main__":
+    main()
