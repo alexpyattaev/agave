61501fd26dc03325de8e31d69ce6138864212b06
Alex Pyattaev <alex.pyattaev@gmail.com>
2026-02-11 10:46:03 +0200

    improve analysis capability of plotter

---
+++ wire_format/anomaly.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from ipaddress import IPv4Address
+
+
+def analyze_duplicates(duplicate_senders:dict[IPv4Address,int],gossip_state:dict[IPv4Address, dict] ):
+    print("== Duplicate senders: top 10 ==")
+    max_offenders = sorted(zip(duplicate_senders.values(), duplicate_senders.keys()), reverse=True)
+    for dups, ip in max_offenders[:10]:
+        row = f"IP:{ip}: {dups}"
+        state = gossip_state.get(ip)
+        if state is not None:
+            row += f"(version:{state['version']} id: {state['identityPubkey']})"
+
+        print(row)
+
+    print("== Duplicates by version ==")
+
+    # version -> total duplicates
+    dups_by_version = defaultdict(int)
+    for ip, dups in duplicate_senders.items():
+        state = gossip_state.get(ip)
+        if state is None:
+            continue
+        version = state.get("version")
+        if version is None:
+            continue
+        dups_by_version[version] += dups
+
+    # print grouped result
+    for version, total_dups in sorted(dups_by_version.items(), key=lambda x: x[1], reverse=True):
+        print(f"version: {version}: {total_dups}")
--- wire_format/plotter.py
+++ wire_format/plotter.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
+import json
+
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import argparse
 from collections import defaultdict, OrderedDict
+
+from matplotlib.backend_bases import Event, MouseEvent
 from matplotlib.widgets import CheckButtons
 import numpy as np
+from ipaddress import IPv4Address
 
+from anomaly import analyze_duplicates
+
+DEBUG_DUP_SENDERS = True
 
 def parse_data(file_name: str):
     # if we are given npy file produced by grepper
@@ -79,7 +87,7 @@ def ready_indicator(dct, shreds_set):
     return indicators
 
 
-def extract_block(block_df) -> pd.DataFrame:
+def extract_block(block_df, leader_schedule:dict, gossip_state:dict) -> pd.DataFrame:
     # tracks the process of FEC set building
     shreds = defaultdict(list)
     # tracks times of arrivals for all duplicates
@@ -92,7 +100,8 @@ def extract_block(block_df) -> pd.DataFrame:
     block_df.loc[:, "fec_index"] = block_df["index"] // 64
     grouped_by_fec = block_df.groupby("fec_index")
     fec_set_indices = pd.unique(block_df["fec_index"])
-
+    fec_set_completion_stats = []
+    duplicate_senders = defaultdict(int)
     for fec_id, group in grouped_by_fec:
         group = group.sort_values("time_stamp")
         # tracks which indices we have received, and records the row number for them
@@ -108,8 +117,10 @@ def extract_block(block_df) -> pd.DataFrame:
             if shred.index in received_indices:
                 base_shred_time = group.loc[received_indices[shred.index], "time_stamp"]
                 base_shred_y = list(received_indices.keys()).index(shred.index)
-                print("dup", base_shred_time, base_shred_y)
+                #print("dup", base_shred_time, base_shred_y)
                 duplicates[fec_id].append((base_shred_time, base_shred_y))
+
+                duplicate_senders[IPv4Address( shred.sender_ip)]+=1
             else:
                 received_indices[shred.index] = shred.Index
 
@@ -118,9 +129,12 @@ def extract_block(block_df) -> pd.DataFrame:
 
             if shred.is_repair:
                 repairs[fec_id].append((shred.time_stamp, y))
+        fec_set_completion_stats.append(len(received_indices.keys()))
 
     def total_shreds(x) -> int:
         return sum([len(fs) for fs in multicasts.values()])
+
+
 
     print(
         f"Block Data Statistics:\n- FEC set count: {len(shreds)}"
@@ -128,8 +142,13 @@ def extract_block(block_df) -> pd.DataFrame:
         f"\n- Multicast shreds: {total_shreds(multicasts)}"
         f"\n- Unique unicast shreds: {total_shreds(shreds) - total_shreds(duplicates)}"
         f"\n- Duplicate unicast shreds: {total_shreds(duplicates)}"
+        f"\n- Shreds per FEC set: E={np.mean(fec_set_completion_stats)} median={np.median(fec_set_completion_stats)}"
         f"\n- Repair shreds in block: {total_shreds(repairs)}"
+
     )
+
+    if DEBUG_DUP_SENDERS:
+        analyze_duplicates(duplicate_senders, gossip_state)
 
     sources = {
         "shreds": shreds,
@@ -165,7 +184,6 @@ def plot_shreds(
     ax.clear()
     colors = mpl.color_sequences["Set1"]
     max_y = 0
-
     zero_time = min(block_df.loc["shreds"].loc["times"].min())
     # from ipaddress import IPv4Address
     # late = df.loc[(df["time_stamp"] - zero_time) > 400000]
@@ -274,8 +292,26 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="data file path", type=str)
     parser.add_argument("slot", help="start at given slot",nargs="?", type=int, default=None)
+    parser.add_argument("--leader_schedule", help="file with leader schedule (json)",
+                        type=str, default=None)
+    parser.add_argument("--gossip_state", help="file with gossip state (json)",
+                        type=str, default=None)
     args = parser.parse_args()
 
+    leader_schedule = {}
+    if args.leader_schedule is not None:
+        with open(args.leader_schedule, "r") as f:
+            leader_schedule = json.load(f)['leaderScheduleEntries']
+            leader_schedule = {ls['slot']: ls['leader'] for ls in leader_schedule}
+
+    gossip_state = {}
+    if args.gossip_state is not None:
+        with open(args.gossip_state, "r") as f:
+            for row in json.load(f):
+                # store same value using both IP address and pubkey
+                # to save sanity when passing this around
+                gossip_state[IPv4Address(row["ipAddress"])] = row
+                gossip_state[row['identityPubkey']] = row
     data = parse_data(args.path)
     all_slots = sorted(pd.unique(data["slot_number"]))
     start_index = all_slots.index(args.slot) if args.slot is not None else 0
@@ -295,11 +331,20 @@ def main():
     )
 
     def render():
-        slot_id = cursor.current()
-        print(f"Slot ID: {slot_id} ")
+        slot_id = int(cursor.current())
+        leader_id = leader_schedule.get(slot_id)
+
+        leader_info = ""
+        if leader_id is not None:
+            leader_info += f" {leader_id}"
+            gossip_info = gossip_state.get(leader_id)
+            if gossip_info is not None:
+                leader_info+= f" {gossip_info['ipAddress']} {gossip_info['version']}"
+        title = f"Block number {slot_id} {leader_info}"
+        print(title)
         block_df = data.loc[data["slot_number"] == slot_id].copy()
 
-        block_df = extract_block(block_df)
+        block_df = extract_block(block_df, leader_schedule, gossip_state)
         # done_batches = when_batch_done(df)
 
         plot_shreds(
@@ -310,7 +355,7 @@ def main():
         )
 
         fig.suptitle(
-            f"Block number {cursor.current()}",
+            title,
             fontsize=14,
             color="white",
         )
@@ -320,17 +365,22 @@ def main():
         visibility_options[label] = not visibility_options[label]
         render()
 
-    def on_press(event):
+    def on_press(event)->None:
         if event.key == "right":
             cursor.next()
         elif event.key == "left":
             cursor.prev()
         elif event.key == "escape":
             exit()
+
         render()
 
     check.on_clicked(check_toggle)
+    def on_mouse_move(event:MouseEvent)->None:
+        #print(event.xdata, event.ydata)
+        pass
 
+    fig.canvas.mpl_connect("motion_notify_event", on_mouse_move)
     fig.canvas.mpl_connect("key_press_event", on_press)
     render()
     plt.show()
